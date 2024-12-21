15aba64d237813895d87e8cffe2c1204c5c96f58
Honor Everhart <honoreverhart@gmail.com>
2024-12-21 12:36:22 -0700

    Final Commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,69 @@
-
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log(puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
-    <>
-      <div>
-        
+    <div className="container">
+      <div className="App">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
       </div>
-    </>
-  )
+      <div>
+        <p>
+          {featPupId && (
+            <div className="singlePuppy">
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </p>
+      </div>
+      <div>
+        <div class="bg">
+          <div id="dog">
+            <div class="head">
+              <div class="mask mask-head">
+                <div class="nose"></div>
+                <div class="eye"></div>
+              </div>
+              <div class="ear"></div>
+            </div>
+
+            <div class="body">
+              <div class="shirt"></div>
+              <div class="mask-body">
+                <div class="leg"></div>
+              </div>
+              <div class="foot"></div>
+            </div>
+            <div class="back-body">
+              <div class="mask-back-body"></div>
+            </div>
+            <div class="mask-tail"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
